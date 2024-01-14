34fb73f4f701122addcb26f889d8f6f47f5c82af
GYoung <ytok1108@aol.com>
2024-01-14 19:37:00 +0900

    stream success a01

--- 02_DB_Rust/cpp_redis_db/a01_redis_db_rust/src/main.rs
+++ 02_DB_Rust/cpp_redis_db/a01_redis_db_rust/src/main.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use redis::{
     from_redis_value,
     streams::{StreamRangeReply, StreamReadOptions, StreamReadReply},
-    Client, Commands, RedisError,
+    AsyncCommands, Client, Commands, RedisError,
 };
 use tokio::time::sleep;
 
@@ -11,11 +11,11 @@ use tokio::time::sleep;
 async fn main() -> Result<(), RedisError> {
     // 1) Create Connection
     let client = Client::open("redis://127.0.0.1/")?;
-    let mut con = client.get_connection()?;
+    let mut con = client.get_tokio_connection().await?;
 
     // 2) Set / Get Key
-    con.set("my_key", b"Hello world!")?;
-    let result: String = con.get("my_key")?;
+    con.set("my_key", b"Hello world!").await?;
+    let result: String = con.get("my_key").await?;
     println!("--> my_key : {} \n", result);
 
     // 3) xadd to redis stream
@@ -23,12 +23,15 @@ async fn main() -> Result<(), RedisError> {
         "my_stream",
         "*",
         &[("name", "name-01"), ("title", "title 01")],
-    )?;
-    let len: i32 = con.xlen("my_stream")?;
+    )
+    .await?;
+    let len: i32 = con.xlen("my_stream").await?;
     println!("-->> my_stream len {} \n", len);
 
     // 4) xrevrange the read stream
-    let result: Option<StreamRangeReply> = con.xrevrange("my_stream", "+", 10)?;
+    let result: Option<StreamRangeReply> = con
+        .xrevrange_count("The first stream", "+", "-", 10)
+        .await?;
     if let Some(reply) = result {
         for stream_id in reply.ids {
             println!("->> xrevrange stream entity: {}", stream_id.id);
@@ -42,12 +45,13 @@ async fn main() -> Result<(), RedisError> {
     // 5) Blocking xread
     tokio::spawn(async {
         let client = Client::open("redis://127.0.0.1/").unwrap();
-        let mut con = client.get_connection().unwrap();
+        let mut con = client.get_tokio_connection().await.unwrap();
 
         loop {
             let opts = StreamReadOptions::default().count(1).block(0);
             let result: Option<StreamReadReply> = con
-                .xread_options(&["The first stream"], &["$"], &opts)
+                .xread_options(&["my_stream"], &["$"], &opts)
+                .await
                 .unwrap();
             if let Some(reply) = result {
                 for stream_key in reply.keys {
@@ -61,22 +65,25 @@ async fn main() -> Result<(), RedisError> {
         }
     });
     // 6) Add some stream entries
+    sleep(Duration::from_millis(100)).await;
     con.xadd(
         "my_stream",
         "*",
         &[("name", "name-02"), ("title", "title 02")],
-    )?;
+    )
+    .await?;
+    sleep(Duration::from_millis(100)).await;
     con.xadd(
         "my_stream",
         "*",
         &[("name", "name-03"), ("title", "title 03")],
-    )?;
-    // sleep(Duration::from_millis(100)).await;
+    )
+    .await?;
 
     // 7) Final wait & cleanup
     sleep(Duration::from_millis(1000)).await;
-    con.del("my_key")?;
-    con.del("my_stream")?;
+    con.del("my_key").await?;
+    con.del("my_stream").await?;
 
     println!("->> the end");
 
